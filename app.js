b19983f4ba7fc0ac50b1e3bf74bbfe71fbcb8758
surendra477 <58383796+surendra477@users.noreply.github.com>
2020-05-02 08:19:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.post("/", function(req, res){
 
   const query = req.body.cityname;
-  const apikey = "e72ca729af228beabd5d20e3b7749713"
+  const apikey = "kfgbsdjkghwugsdmgwej"
   const unit = "metric"
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query +  "&appid=" + apikey + "&units=" + unit;
 
